420bea0b4023a434863a80b82703fa71e6fe2efd
Jason San Jose <jasonsj@adobe.com>
2013-05-03 15:53:59 -0700

    fix setup bugs

--- scripts/setup_linux_build.sh
+++ scripts/setup_linux_build.sh
@@ -14,25 +14,30 @@ fi
 rm -rf cef
 unzip cef.zip
 mv $zipname cef
+./make_symlinks.sh
 
 # get libudev0 for Ubuntu 13.04
 if egrep -q "13\.04" /etc/issue; then
-  wget -O libudev0_175-0ubuntu19_i386.deb http://launchpadlibrarian.net/132294322/libudev0_175-0ubuntu19_i386.deb
-  dpkg -i *.deb 
-  apt-get install -f
+  if [ ! -f libudev0.deb ]; then
+    wget -O libudev0.deb http://launchpadlibrarian.net/132294322/libudev0_175-0ubuntu19_i386.deb
+  fi
+
+  sudo dpkg -i *.deb 
+  sudo apt-get install -f
 fi
 
 # download chromium dependency script
 if [ ! -f install-build-deps.sh ]; then
-  wget -O install-build-deps.sh http://src.chromium.org/svn/trunk/src/build/install-build-deps.sh
+  wget -O install-build-deps.sh https://gist.github.com/jasonsanjose/5514813/raw/8f333053a1a1180c2bfd4aabca94d4cff76e8595/install-build-deps.sh
   chmod 755 install-build-deps.sh
 fi
 
 # install dependecies
-install-build-deps.sh --no-chromeos-fonts
+./install-build-deps.sh --no-chromeos-fonts
 
 popd
 
 # install and run gyp to create makefile
-apt-get install gyp
+sudo apt-get install gyp
+
 gyp --depth .
